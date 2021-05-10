7d5404d50eccb538e5384028caaa6f421607eab6
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-10 11:51:53 +0300

    feat: add Popup component

---
+++ src/components/Popup/Popup.ts
@@ -0,0 +1,38 @@
+import './Popup.sass';
+
+import Control from '../Control/Control';
+
+interface IPopup {
+  name: string;
+  popup: Control;
+
+  hidePopup(e: Event): void;
+  showPopup(): void;
+}
+
+export default class Popup extends Control implements IPopup {
+  popup: Control;
+
+  name: string;
+
+  constructor(name: string) {
+    super({ parentNode: document.body, className: `${name}-popup__wrapper popup__wrapper` });
+    this.render();
+
+    this.name = name;
+    this.popup = new Control({ parentNode: this.node, className: `${name}-popup popup` });
+
+    this.state.setState({ isOpen: false });
+    this.node.addEventListener('click', this.hidePopup);
+  }
+
+  hidePopup = (e: Event): void => {
+    if (e.target === this.node) this.node.classList.remove('active');
+    this.state.setState({ isOpen: false });
+  };
+
+  showPopup = (): void => {
+    this.node.classList.add('active');
+    this.state.setState({ isOpen: true });
+  };
+}
