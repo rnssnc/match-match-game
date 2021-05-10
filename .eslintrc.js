c55da07f6dbbfff75766d89d429d75ab95001567
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-10 11:49:00 +0300

    refactor: config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,14 @@
 module.exports = {
   env: {
     browser: true,
-    commonjs: true,
     es2021: true,
   },
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-typescript/base', 'eslint:recommended', 'plugin:@typescript-eslint/recommended'],
   parserOptions: {
-    ecmaVersion: 12,
+    project: './tsconfig.json',
   },
-  plugins: [],
-  rules: {
-    'import/no-extraneous-dependencies': [
-      'error',
-      {
-        devDependencies: true,
-      },
-    ],
-  },
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint'],
+  ignorePatterns: ['**/webpack/*.js'],
+  rules: {},
 };
