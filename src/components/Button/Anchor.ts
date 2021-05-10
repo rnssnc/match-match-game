28bed7a0104f399042196ab5fcdf5658ede16450
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-10 11:49:24 +0300

    feat: add button component

---
+++ src/components/Button/Anchor.ts
@@ -0,0 +1,31 @@
+import AnchorButton, { IAnchorButton, ButtonTypes, ButtonModifiers } from './AnchorButton';
+
+export interface IAnchor extends IAnchorButton {
+  node: HTMLAnchorElement;
+}
+
+export interface IAnchorSettings extends IAnchorButton {
+  data: { value?: string; href?: string };
+}
+
+export default class Anchor extends AnchorButton implements IAnchor {
+  node!: HTMLAnchorElement;
+
+  constructor(
+    parentNode: HTMLElement,
+    className: string,
+    data?: { value?: string; href?: string },
+    options?: { type?: ButtonTypes; modifier?: ButtonModifiers },
+  ) {
+    const defaultData = { href: '#', ...data };
+
+    super(parentNode, 'a', `button ${className}`, defaultData, options);
+
+    if (data && data.href) this.setHref(data.href);
+  }
+
+  setHref(newHref: string): void {
+    this.node.href = newHref;
+    this.state.setState({ href: newHref });
+  }
+}
