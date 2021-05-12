1931793d5330784263b79ee08aad54fb25b7b4c0
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-12 20:08:23 +0300

    feat: add app page layout

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,3 +1,30 @@
-import 'normalize.css';
-
+import '../base/base';
 import './index.sass';
+
+import Header from '../../components/Header/Header';
+
+import ScreenRouter from '../../components/ScreenRouter/ScreenRouter';
+import AboutScreen from '../../components/Screen/AboutScreen/AboutScreen';
+import GameScreen from '../../components/Screen/GameScreen/GameScreen';
+
+interface IMatchMatchGame {
+  header: Header;
+}
+
+const routes = [
+  { Screen: AboutScreen, id: 'about' },
+  { Screen: GameScreen, id: 'game' },
+];
+
+class Application implements IMatchMatchGame {
+  header: Header;
+
+  screenRouter: ScreenRouter;
+
+  constructor() {
+    this.header = new Header(document.body);
+    this.screenRouter = new ScreenRouter({ parentNode: document.body, routes, startRouteId: 'about' });
+  }
+}
+
+const app = new Application();
