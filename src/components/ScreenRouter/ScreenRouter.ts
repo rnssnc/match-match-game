9ff7c5db6bd70bab78c4d6dafe7f863d2b95c6ab
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-30 02:50:08 +0300

    feat: update screenRouter for dabase & finalScore

--- src/components/ScreenRouter/ScreenRouter.ts
+++ src/components/ScreenRouter/ScreenRouter.ts
@@ -1,10 +1,16 @@
 import Control from '../Control/Control';
+import Database from '../Database/Database';
 import Screen from '../Screen/Screen';
+import Anchor from '../Button/Anchor';
+import FinalScorePopup from '../Popup/FinalScorePopup/FinalScorePopup';
 
 type Options = {
   parentNode: HTMLElement;
   routes: Array<{ Screen: typeof Screen; id: string }>;
   startRouteId: string;
+  database: Database;
+  navigationItems?: Anchor[];
+  finalScorePopup: FinalScorePopup;
 };
 
 export default class Router extends Control {
@@ -14,18 +20,32 @@ export default class Router extends Control {
 
   loadedRoutes: Screen[];
 
+  database: Database;
+
+  navigationItems!: Anchor[];
+
+  finalScorePopup: FinalScorePopup;
+
   constructor(options: Options) {
     super({ parentNode: options.parentNode, tagName: 'main', className: 'router-root' });
 
     this.loadedRoutes = [];
 
     this.routes = options.routes;
-    // this.loadedRoutes.push(route);
-    options.routes.forEach((Route) => {
-      if (options.startRouteId && options.startRouteId === Route.id) {
-        window.location.hash = Route.id;
-      }
-    });
+
+    if (!window.location.hash || window.location.hash === '#') {
+      options.routes.forEach((Route) => {
+        if (options.startRouteId && options.startRouteId === Route.id) {
+          window.location.hash = Route.id;
+        }
+      });
+    }
+
+    this.finalScorePopup = options.finalScorePopup;
+
+    if (options.navigationItems) this.navigationItems = options.navigationItems;
+
+    this.database = options.database;
 
     window.addEventListener('hashchange', this.showRoute);
 
@@ -38,9 +58,23 @@ export default class Router extends Control {
     const nextRoute = this.routes.find((route) => route.id === hash);
     if (nextRoute) {
       if (this.currentRoute) this.currentRoute.removeScreen();
-      this.currentRoute = new nextRoute.Screen(this.node);
+
+      this.currentRoute = new nextRoute.Screen({
+        parentNode: this.node,
+        database: this.database,
+        finalScorePopup: this.finalScorePopup,
+      });
 
       this.currentRoute.showScreen();
     }
+
+    this.setActiveNavItem(hash);
   };
+
+  setActiveNavItem(hash: string): void {
+    this.navigationItems.forEach((item) => {
+      if (item.state.data?.href === `#${hash}`) item.node.classList.add('button--active');
+      else item.node.classList.remove('button--active');
+    });
+  }
 }
