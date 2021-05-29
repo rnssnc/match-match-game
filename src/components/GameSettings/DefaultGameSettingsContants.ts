831fcb32822058ecec4ac045ff60be297e312c17
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-30 02:51:09 +0300

    feat: add game settings

---
+++ src/components/GameSettings/DefaultGameSettingsContants.ts
@@ -0,0 +1,31 @@
+type CardTheme = {
+  name: string;
+  jsonPath: string;
+};
+
+export const CardsThemes: CardTheme[] = [
+  { name: 'animals', jsonPath: '../../assets/themes/animals.json' },
+  { name: 'test', jsonPath: '' },
+  { name: 'test1', jsonPath: '' },
+];
+
+type CardPattern = {
+  name: string;
+  srcPath: string;
+};
+
+export const CardsPatterns: CardPattern[] = [
+  { name: 'cloud', srcPath: './images/cardFaces/pattern1.svg' },
+];
+
+export type TGameSettings = {
+  fieldSize: { columns: number; rows: number };
+  cardTheme: CardTheme;
+  cardPattern: CardPattern;
+};
+
+export const DEFAULT_GAME_SETTINGS: TGameSettings = {
+  fieldSize: { columns: 4, rows: 3 },
+  cardTheme: CardsThemes[0],
+  cardPattern: CardsPatterns[0],
+};
