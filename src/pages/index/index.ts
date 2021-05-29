1777b8f475448e4030abc426af920cf1f8a2928a
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-30 02:48:09 +0300

    feat: add new screens to app

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -1,30 +1,59 @@
 import '../base/base';
 import './index.sass';
 
+import Database from '../../components/Database/Database';
+
 import Header from '../../components/Header/Header';
 
 import ScreenRouter from '../../components/ScreenRouter/ScreenRouter';
 import AboutScreen from '../../components/Screen/AboutScreen/AboutScreen';
 import GameScreen from '../../components/Screen/GameScreen/GameScreen';
+import RecordScreen from '../../components/Screen/RecordsScreen/RecordsScreen';
+import SettingsScreen from '../../components/Screen/SettingsScreen/SettingsScreen';
+import FinalScorePopup from '../../components/Popup/FinalScorePopup/FinalScorePopup';
 
-interface IMatchMatchGame {
+interface IApplication {
+  rootElement: HTMLElement;
   header: Header;
 }
 
 const routes = [
   { Screen: AboutScreen, id: 'about' },
+  { Screen: RecordScreen, id: 'records' },
+  { Screen: SettingsScreen, id: 'settings' },
   { Screen: GameScreen, id: 'game' },
 ];
 
-class Application implements IMatchMatchGame {
+class Application implements IApplication {
   header: Header;
 
   screenRouter: ScreenRouter;
 
-  constructor() {
-    this.header = new Header(document.body);
-    this.screenRouter = new ScreenRouter({ parentNode: document.body, routes, startRouteId: 'about' });
+  database: Database;
+
+  readonly rootElement: HTMLElement;
+
+  finalScorePopup: FinalScorePopup;
+
+  constructor(rootElement: HTMLElement) {
+    this.rootElement = rootElement;
+
+    this.database = new Database();
+    this.database.init('rnssnc', 1);
+
+    this.header = new Header(rootElement, this.database);
+
+    this.finalScorePopup = new FinalScorePopup();
+
+    this.screenRouter = new ScreenRouter({
+      parentNode: rootElement,
+      routes,
+      startRouteId: 'about',
+      database: this.database,
+      navigationItems: this.header.navigation.items,
+      finalScorePopup: this.finalScorePopup,
+    });
   }
 }
 
-const app = new Application();
+const app = new Application(document.body);
