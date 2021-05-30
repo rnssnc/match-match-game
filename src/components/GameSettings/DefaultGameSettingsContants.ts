5e167cfc281b5952fec9b49a025d2fc9e9c65913
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-30 14:37:00 +0300

    refactor: field sizes

--- src/components/GameSettings/DefaultGameSettingsContants.ts
+++ src/components/GameSettings/DefaultGameSettingsContants.ts
@@ -3,10 +3,23 @@ type CardTheme = {
   jsonPath: string;
 };
 
-export const CardsThemes: CardTheme[] = [
+type FieldSize = {
+  name: string;
+  size: { columns: number; rows: number };
+};
+
+export const CARD_THEMES: CardTheme[] = [
   { name: 'animals', jsonPath: '../../public/themes/animals.json' },
-  { name: 'test', jsonPath: '' },
-  { name: 'test1', jsonPath: '' },
+  { name: 'cosmetic', jsonPath: '../../public/themes/cosmetic.json' },
+  { name: 'nature', jsonPath: '../../public/themes/nature.json' },
+  { name: 'origami', jsonPath: '../../public/themes/origami.json' },
+];
+
+export const FIELD_SIZES: FieldSize[] = [
+  { name: '2x2', size: { columns: 2, rows: 2 } },
+  { name: '4x4', size: { columns: 4, rows: 4 } },
+  { name: '6x6', size: { columns: 6, rows: 6 } },
+  { name: '8x8', size: { columns: 8, rows: 8 } },
 ];
 
 type CardPattern = {
@@ -14,18 +27,18 @@ type CardPattern = {
   srcPath: string;
 };
 
-export const CardsPatterns: CardPattern[] = [
+export const CARD_PATTERNS: CardPattern[] = [
   { name: 'cloud', srcPath: './images/cardFaces/pattern1.svg' },
 ];
 
 export type TGameSettings = {
-  fieldSize: { columns: number; rows: number };
+  fieldSize: FieldSize;
   cardTheme: CardTheme;
   cardPattern: CardPattern;
 };
 
 export const DEFAULT_GAME_SETTINGS: TGameSettings = {
-  fieldSize: { columns: 4, rows: 3 },
-  cardTheme: CardsThemes[0],
-  cardPattern: CardsPatterns[0],
+  fieldSize: FIELD_SIZES[0],
+  cardTheme: CARD_THEMES[0],
+  cardPattern: CARD_PATTERNS[0],
 };
