70672a09cfcbc865c25d7e0bacd1e7d1172c5fc0
eminov.rnssnc <eminov.rnssnc@gmail.com>
2021-05-30 14:46:26 +0300

    feat: add timer delay skip

--- src/components/Screen/GameScreen/GameScreen.ts
+++ src/components/Screen/GameScreen/GameScreen.ts
@@ -7,12 +7,14 @@ import Timer from '../../Timer/Timer';
 import Game from '../../Game/Game';
 import GameSettings from '../../GameSettings/GameSettings';
 import FinalScorePopup from '../../Popup/FinalScorePopup/FinalScorePopup';
+import Header from '../../Header/Header';
 
 type Options = {
   parentNode: HTMLElement;
   className: string;
   database: Database;
   finalScorePopup: FinalScorePopup;
+  header: Header;
 };
 
 export default class GameScreen extends Screen {
@@ -24,6 +26,10 @@ export default class GameScreen extends Screen {
 
   currentUser!: DatabaseRecord;
 
+  descriptionContent!: Control;
+
+  timerMessage!: Control;
+
   constructor(options: Options) {
     super({
       parentNode: options.parentNode,
@@ -31,13 +37,30 @@ export default class GameScreen extends Screen {
       className: 'game-screen',
       database: options.database,
       finalScorePopup: options.finalScorePopup,
+      header: options.header,
     });
 
-    if (!this.database.currentUser) {
-      alert('You need to register first :) Sorry I didn`t have enought time for nice popup');
-      return;
-    }
-    this.timer = new Timer(this.node, 'game-timer');
+    // if (!this.database.currentUser) {
+    //   alert('You need to register first :) Sorry I didn`t have enought time for nice popup');
+    //   return;
+    // }
+
+    this.header.startGameButton.hide();
+    this.header.stopGameButton.show();
+
+    this.descriptionContent = new Control({
+      parentNode: this.node,
+      className: 'game_-description-content',
+    });
+
+    this.timer = new Timer(this.descriptionContent.node, 'game-timer');
+
+    this.timerMessage = new Control({
+      parentNode: this.descriptionContent.node,
+      className: 'timer__countdown-message',
+      content:
+        'You have 30 seconds to keep cards in mind. Click on field earlier if you are ready.',
+    });
 
     this.gameSettings = new GameSettings();
 
@@ -51,7 +74,10 @@ export default class GameScreen extends Screen {
       winCallback: (stats) => {
         this.currentUser.score = stats.score;
 
-        this.database.put(this.currentUser).then(() => this.finalScorePopup.show(stats));
+        this.database.put(this.currentUser).then(() => this.finalScorePopup.display(stats));
+      },
+      startCallback: () => {
+        this.timerMessage.node.classList.add('timer-message--hidden');
       },
     });
   }
